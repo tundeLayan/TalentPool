355bfff621f8f88c05b12247c6d5d553cfea798c
Frost199 <emmaldini12@gmail.com>
2020-07-21 20:41:08 +0100

    feat: Add test sample

---
+++ test/demo_spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Unit testing demo', () => {
+  describe('basic testing', () => {
+    it('should pass', () => {
+      assert(true);
+    });
+  });
+});
