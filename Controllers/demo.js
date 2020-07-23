d164208d481358b7d9f84227355613752c55dbfb
Frost199 <emmaldini12@gmail.com>
2020-07-23 09:56:06 +0100

    feat: abstracted res.render

--- Controllers/demo.js
+++ Controllers/demo.js
@@ -1,5 +1,12 @@
+const { renderPage } = require('../Utils/render-page');
+
 const demo = (req, res) => {
-  res.render('index', { title: 'Express' });
+  const data = {
+    message: 'This is an example',
+    firstName: 'John',
+    lastName: 'Doe'
+  }
+  renderPage(res, 'index', '/', 'Demo Page', data)
 };
 
 module.exports.demo = demo;
