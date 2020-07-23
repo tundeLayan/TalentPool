9b3b4729c0a7fd45652d58fb353388242fe51244
tobilasinde <tobilasinde2012@yahoo.com>
2020-07-23 07:30:08 +0100

    Employer Dashboard BE

--- Controllers/employer/dashboard.js
+++ Controllers/employer/dashboard.js
@@ -1,7 +1,21 @@
-const dashboard = (req, res) => {
-    
-    res.render('index', { title: 'Express' });
-  };
+const model = require('../../Models');
+
+const dashboard = async (req, res) => {
+  try {
+    const recommendedInterns = await model.Employee.findAll();
+    const pendingHire = await model.Team.findAll({
+      where: {
+        // TODO: get the session data from auth
+        userId: 1,
+      },
+    });
+    const data = { recommendedInterns, pendingHire }
+    res.render('employer/employerDashboard', { data, title: 'Employer Dashboard' });
+  } catch (err) {
+    const error = err;
+    const { message } = err;
+    res.render('error', { message, error });
+  }
+};
   
   module.exports.dashboard = dashboard;
-  
