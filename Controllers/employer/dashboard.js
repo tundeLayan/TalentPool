4ad4a83d7c24253a97b0bfe9f1033bf55621678c
tobilasinde <tobilasinde2012@yahoo.com>
2020-07-23 09:58:07 +0100

    Employer dashboard Controller

--- Controllers/employer/dashboard.js
+++ Controllers/employer/dashboard.js
@@ -2,14 +2,30 @@ const model = require('../../Models');
 
 const dashboard = async (req, res) => {
   try {
-    const recommendedInterns = await model.Employee.findAll();
+    const recommendedInterns = await model.Employee.findAll({
+      where: {
+        userType: 'HNG',
+        verificationStatus: 'Approved'
+      }
+    });
+    const allEmployee = await model.Employee.findAll({
+      where: {
+        verificationStatus: 'Approved'
+      }
+    });
     const pendingHire = await model.Team.findAll({
       where: {
-        // TODO: get the session data from auth
-        userId: 1,
+        userId: req.session.userId,
+        status: 'Pending'
       },
     });
-    const data = { recommendedInterns, pendingHire }
+    const teamMember = await model.Team.findAll({
+      where: {
+        userId: req.session.userId,
+        status: 'Accepted'
+      },
+    });
+    const data = { allEmployee, teamMember, recommendedInterns, pendingHire }
     res.render('employer/employerDashboard', { data, title: 'Employer Dashboard' });
   } catch (err) {
     const error = err;
