072d02617e8c806c9e8f239eaaf81899de191ef1
thebolarin <odutusinmoses@gmail.com>
2020-07-24 22:42:01 +0100

    fixed

--- Utils/response.js
+++ Utils/response.js
@@ -22,31 +22,33 @@ const errorUserSignup = (
   });
 
 
-module.exports.errorUserSignup = errorUserSignup;
 
-// module.exports = {
-//   authErrorRedirect: (
-//     req,
-//     res,
-//     email,
-//     password,
-//     errorMessage,
-//     page,
-//     title,
-//     pagePath,
-//   ) => {
-//     const { isLoggedIn } = req.session;
-//     return res.status(401).render(`${page}`, {
-//       path: `${pagePath}`,
-//       pageName: `${title}`,
-//       errorMessage,
-//       isLoggedIn,
-//       success: req.flash('success'),
-//       oldInput: {
-//         email,
-//         password,
-//       },
-//       validationErrors: [],
-//     });
-//   },
-// };
+
+module.exports = {
+  authErrorRedirect: (
+    req,
+    res,
+    email,
+    password,
+    errorMessage,
+    page,
+    title,
+    pagePath,
+  ) => {
+    const { isLoggedIn } = req.session;
+    return res.status(401).render(`${page}`, {
+      path: `${pagePath}`,
+      pageName: `${title}`,
+      errorMessage,
+      isLoggedIn,
+      success: req.flash('success'),
+      oldInput: {
+        email,
+        password,
+      },
+      validationErrors: [],
+    });
+  },
+};
+
+module.exports.errorUserSignup = errorUserSignup;
