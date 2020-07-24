a6cded74004188450a48837f6ffdd5e7707a1b71
linda <lindatochukwu30@gmail.com>
2020-07-24 14:18:25 +0100

    controllers modified

--- Controllers/external-pages/home.js
+++ Controllers/external-pages/home.js
@@ -6,11 +6,8 @@ const home = (req, res) => {
     firstName: 'John',
     lastName: 'Doe'
   }
-<<<<<<< HEAD
-  renderPage(res, 'employer/employerProfileSettings', data, 'Demo Page')
-=======
+
   renderPage(res, 'index', data, 'Demo Page')
->>>>>>> 9a6dcf5fa6192231ccf63163e43a49cf24693c3b
 };
 
 module.exports.home = home;
