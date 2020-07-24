b59f097caab8daad957c5c38590bb46d37320792
Ibrahim Adekunle <adefemi101@gmail.com>
2020-07-24 13:05:34 +0100

    fix: added a federalized handler for  auth errors.

--- Utils/validators/login-validator.js
+++ Utils/validators/login-validator.js
@@ -1,13 +1,22 @@
 /* eslint-disable consistent-return */
 const { validationResult } = require('express-validator');
-const { errorUserLogin } = require('../response');
+const { authErrorRedirect } = require('../response');
 
 module.exports = {
   validateUserRequest: (req, res, email, password) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const errorMessage = errors.array()[0].msg;
-      return errorUserLogin(req, res, email, password, errorMessage);
+      return authErrorRedirect(
+        req,
+        res,
+        email,
+        password,
+        errorMessage,
+        'auth/login',
+        'Login',
+        '/login',
+      );
     }
   },
 };
