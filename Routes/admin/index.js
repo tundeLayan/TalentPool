4d53a8aafc84b5988a5c8df0206afac6de81170d
youngGaf <youngabuhafiz@gmail.com>
2020-07-25 13:15:08 +0100

    style: remove route implementation for deleted file

--- Routes/admin/index.js
+++ Routes/admin/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const faqRoute = require('./faq')
 const dashboard = require('./dashboard');
 const employer = require('./employer');
-const verification = require('./verification');
 
 const router = express.Router();
 router.use('/faq', faqRoute)
 router.use(dashboard);
 router.use(employer);
-router.use(verification);
+
 
 module.exports = router
