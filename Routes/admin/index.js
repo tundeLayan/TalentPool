67903d1c0d8e9c7451a994e258c86746373030f4
Frost199 <emmaldini12@gmail.com>
2020-07-25 23:19:09 +0100

    fix: routing on roles validation

--- Routes/admin/index.js
+++ Routes/admin/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const admin = require('./admin');
 const faqRoute = require('./faq');
 const dashboard = require('./dashboard');
+const { authorisedPages } = require('../../Middleware/auth');
 
-router.use(dashboard);
-router.use(admin);
-router.use('/faq', faqRoute);
+const roleSuperAdmin = 'ROL-SUPERADMIN';
+const roleAdmin = 'ROL-ADMIN';
+
+router.use('/dashboard', authorisedPages(roleAdmin, roleSuperAdmin), dashboard);
+router.use('/', authorisedPages(roleAdmin, roleSuperAdmin), admin);
+router.use('/faq', authorisedPages(roleAdmin, roleSuperAdmin), faqRoute);
 
 module.exports = router;
