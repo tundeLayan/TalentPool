d737039e968395b929de90be116d900bcac09dc0
Taiwo Adejare <adejareemma@gmail.com>
2020-07-25 20:02:25 +0100

    feat: Add Admin

--- Routes/admin/admin.js
+++ Routes/admin/admin.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = express.Router();
-const { getAllAdmin, blockAdmin, unblockAdmin, getAdminFullDetails } = require('../../Controllers/admin/admin');
+const { getAllAdmin, blockAdmin, unblockAdmin, getAdminFullDetails, addAdmin } = require('../../Controllers/admin/admin');
 
 route.get('/all', getAllAdmin);
+route.post('/add', addAdmin)
 route.get('/profile/:userId', getAdminFullDetails);
 route.patch('/block/:userId', blockAdmin);
 route.patch('/unblock/:userId', unblockAdmin);
