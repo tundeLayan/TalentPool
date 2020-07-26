728f59ed863a8680b44845e6a86084db24a09e12
Sarah Embee <sarahembee71@gmail.com>
2020-07-26 15:40:02 +0100

    Employer Dashboard

--- Controllers/employer/dashboard.js
+++ Controllers/employer/dashboard.js
@@ -6,6 +6,10 @@ const {
   getRecommendedInterns,
   getPendingHire,
   getTeamMember,
+  getBEinTeam,
+  getFEinTeam,
+  getDesigninTeam,
+  getMobileinTeam,
 } = require('../dao/db-queries');
 
 const dashboard = async (req, res) => {
@@ -14,7 +18,11 @@ const dashboard = async (req, res) => {
     const recommendedInterns = await getRecommendedInterns(db);
     const pendingHire = await getPendingHire(req, db);
     const teamMember = await getTeamMember(req, db);
-    const data = { allEmployee, teamMember, recommendedInterns, pendingHire };
+    const backEnd = await getBEinTeam(req, db);
+    const frontEnd = await getFEinTeam(req, db);
+    const design = await getDesigninTeam(req, db);
+    const mobile = await getMobileinTeam(req, db);
+    const data = { allEmployee, teamMember, recommendedInterns, pendingHire, backEnd, frontEnd, design, mobile };
     renderPage(
       res,
       'employer/employerDashboard',
